08852461da589886dd773e0e97b0b298108ff61e
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2022-01-10 23:28:02 +0100

    Added configuration file

---
+++ cronjob/configuration.php
@@ -0,0 +1,13 @@
+<?php
+// How often we are checking
+// Value bigger then this will be logged
+$minutes = 1;
+
+// How much seconds to tolerate
+$tolerate_time = 2;
+
+// Where to log time of last check
+$check_file = '../used/uptime_check.dat';
+
+// Where to log downtime
+$check_log = '../used/downtime_log.dat';
